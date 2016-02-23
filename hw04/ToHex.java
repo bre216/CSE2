5e9931305a7c4e951aa3ced03ecdc4fb1c1db789
bre216 <bre216@lehigh.edu>
2016-02-23 01:56:34 +0000

    2nd update hw04

--- hw04/ToHex.java
+++ hw04/ToHex.java
@@ -15,19 +15,17 @@ public class ToHex{ //declares class
         int redValue=myScanner.nextInt(); //assigns first integer as the red value 
         int greenValue=myScanner.nextInt(); //assigns second integer as green value
         int blueValue=myScanner.nextInt(); //assigns third integer as blue value
-        
+                    
         String hexdec1=""; //declares string variable for 1st hexadecimal digit
         String hexdec2=""; //declares string variable for 2nd hexadecimal digit
         String hexdec3=""; //declares string variable for 3rd hexadecimal digit
         String hexdec4=""; //declares string variable for 4th hexadecimal digit
         String hexdec5=""; //declares string variable for 5th hexadecimal digit
         String hexdec6=""; //declares string variable for 6th hexadecimal digit
-        String hexdec=""+hexdec1+hexdec2+hexdec3+hexdec4+hexdec5+hexdec6; //declares string variable for combined hexadecimal 
         
         if( redValue<=255 && redValue>=0 ){ //checks to see if the first value is between 0-255
             if( greenValue<=255 && greenValue>=0 ){ //checks to see if the second value is between 0-255
                 if( blueValue<=255 && blueValue>=0 ){ //checks to see if the third value is between 0-255
-                    
                     
                     int redDivided=redValue/16; //divides input for red by 16 and saves the answer as an integer value
                     int redRemainder=redValue%16; //divides the input for red by 16 and saves the remainder as an integer value 
@@ -37,11 +35,11 @@ public class ToHex{ //declares class
                     int blueRemainder=blueValue%16; //divides the input for blue by 16 and saves the remainder as an integer value 
                     
                     //convert first red digit to hexadecimal 
-                    if( redDivided<10 ){
-                        hexdec1=""+redDivided;
+                    if( redDivided<10 ){ 
+                        hexdec1=""+redDivided; //if red input is divisible by 16 less than 10 times, assigns first hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( redDivided ){
+                        switch( redDivided ){ //if red input is divisible by 16 more than 10 times, assigns first hexadecimal digit to a letter based on the integer number of divisions
                             case 10:
                                 hexdec1="A";
                                 break;
@@ -65,10 +63,10 @@ public class ToHex{ //declares class
                     
                     //convert second red digit to hexadecimal 
                     if( redRemainder<10 ){
-                        hexdec2=""+redRemainder;
+                        hexdec2=""+redRemainder; //if remainder after dividing red input by 16 is less than 10, assigns first hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( redRemainder ){
+                        switch( redRemainder ){ //if remainder after dividing red input by 16 is more than 10, assigns first hexadecimal digit to a letter based on remainder
                             case 10:
                                 hexdec2="A";
                                 break;
@@ -92,10 +90,10 @@ public class ToHex{ //declares class
                     
                     //convert first green digit to hexadecimal 
                     if( greenDivided<10 ){
-                        hexdec3=""+greenDivided;
+                        hexdec3=""+greenDivided; //if green input is divisible by 16 less than 10 times, assigns third hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( greenDivided ){
+                        switch( greenDivided ){ //if green input is divisible by 16 more than 10 times, assigns third hexadecimal digit to a letter based on the integer number of divisions
                             case 10:
                                 hexdec3="A";
                                 break;
@@ -119,10 +117,10 @@ public class ToHex{ //declares class
                     
                     //convert second green digit to hexadecimal 
                     if( greenRemainder<10 ){
-                        hexdec4=""+greenRemainder;
+                        hexdec4=""+greenRemainder; //if remainder after dividing green input by 16 is less than 10, assigns fourth hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( greenRemainder ){
+                        switch( greenRemainder ){ //if remainder after dividing green input by 16 is more than 10, assigns fourth hexadecimal digit to a letter based on remainder
                             case 10:
                                 hexdec4="A";
                                 break;
@@ -146,10 +144,10 @@ public class ToHex{ //declares class
                     
                     //convert first blue digit to hexadecimal 
                     if( blueDivided<10 ){
-                        hexdec5=""+blueDivided;
+                        hexdec5=""+blueDivided; //if blue input is divisible by 16 less than 10 times, assigns fifth hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( blueDivided ){
+                        switch( blueDivided ){ //if blue input is divisible by 16 more than 10 times, assigns fifth hexadecimal digit to a letter based on the integer number of divisions
                             case 10:
                                 hexdec5="A";
                                 break;
@@ -173,10 +171,10 @@ public class ToHex{ //declares class
                     
                     //convert second blue digit to hexadecimal 
                     if( blueRemainder<10 ){
-                        hexdec6=""+blueRemainder;
+                        hexdec6=""+blueRemainder; //if remainder after dividing blue input by 16 is less than 10, assigns sixth hexadecimal digit to the integer number of divisions
                     }
                     else{
-                        switch( blueRemainder ){
+                        switch( blueRemainder ){ //if remainder after dividing red input by 16 is more than 10, assigns sixth hexadecimal digit to a letter based on remainder
                             case 10:
                                 hexdec6="A";
                                 break;
@@ -197,18 +195,19 @@ public class ToHex{ //declares class
                                 break;
                         }
                     }
-                    System.out.print("The decimal numbers R:"+redValue+", G:"+greenValue+", B:"+blueValue+", is represented in hexadecimal as:"+hexdec1+hexdec2+hexdec3+hexdec4+hexdec5+hexdec6);
+                    String hexdec=""+hexdec1+hexdec2+hexdec3+hexdec4+hexdec5+hexdec6; //declares string variable for combined hexadecimal 
+                    System.out.print("The decimal numbers R:"+redValue+", G:"+greenValue+", B:"+blueValue+", is represented in hexadecimal as:"+hexdec); //prints output
                 }
                 else{
-                    System.out.print("Sorry, you must enter values between 0-255"); //tells user to input values within acceptable range
+                    System.out.print("Sorry, you must enter values between 0-255."); //tells user to input values within acceptable range
                 }
             }
             else{
-                System.out.print("Sorry, you must enter values between 0-255"); //tells user to input values within acceptable range
+                System.out.print("Sorry, you must enter values between 0-255."); //tells user to input values within acceptable range
             }
         }
         else{
-            System.out.print("Sorry, you must enter values between 0-255"); //tells user to input values within acceptable range
+            System.out.print("Sorry, you must enter values between 0-255."); //tells user to input values within acceptable range
         }
     }
 }
