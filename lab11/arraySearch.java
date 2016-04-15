e59c0a68bf09a218ac4241f2d03b116f13baeee8
bre216 <bre216@lehigh.edu>
2016-04-15 20:34:00 +0000

    updated lab11

--- lab11/arraySearch.java
+++ lab11/arraySearch.java
@@ -18,15 +18,6 @@ public class arraySearch{
             array1[i]=(int)(Math.random()*101);
         } 
         
-        //use linear search to find maximum of array1
-        int max1=0; 
-        for( int i=0; i<50; i++ ){
-            if( array1[i]>max1 ){
-                max1=array1[i];
-            }
-        }
-        System.out.println("The maximum of array1 is: "+max1);
-        
         //use linear search to find the minimum of array1
         int min1=100;
         for( int i=0; i<50; i++ ){
@@ -35,6 +26,15 @@ public class arraySearch{
             }
         }
         System.out.println("The minimum of array1 is: "+min1);
+        
+        //use linear search to find maximum of array1
+        int max1=0; 
+        for( int i=0; i<50; i++ ){
+            if( array1[i]>max1 ){
+                max1=array1[i];
+            }
+        }
+        System.out.println("The maximum of array1 is: "+max1);
         
         //add increasing numbers to array2
         array2[0]=(int)(Math.random()*101);
@@ -63,34 +63,40 @@ public class arraySearch{
         
         //ask user to enter integer >=0
         System.out.println("Please enter an integer greater than or equal to 0: ");
-        if( !sc.hasNextInt() || sc.nextInt()<0 ){
-            System.out.println("You did not enter an integer >= 0.");
+        if( !sc.hasNextInt() ){
+            System.out.println("You did not enter an integer");
             System.exit(0);
         }
         else{
             int input=sc.nextInt();
-            int arrayMax=49;
-            int arrayMin=0;
-            while( true ){
-                
-                if( array2[arrayMax-(arrayMax-arrayMin)/2]==input ){
-                    System.out.println("Your input matched array2 index "+array2[arrayMax-(arrayMax-arrayMin)/2]);
-                    break;
-                }
-                else if( array2[arrayMax-((arrayMax-arrayMin)/2)]>input ){
-                    arrayMax=arrayMax-((arrayMax-arrayMin)/2);
-                    //arrayMin=0;
-                }
-                else if( array2[arrayMax-((arrayMax-arrayMin)/2)]<input ){
-                    //arrayMax=49;
-                    arrayMin=arrayMax-((arrayMax-arrayMin)/2);
-                }
-                if( ( (arrayMax-arrayMin)/2)==0 ){
-                    System.out.println(input+" was not found in the list.");
-                    System.out.println("The number above the input was "+array2[arrayMax]);
-                    System.out.println("The number below the input was "+array2[arrayMin]);
-                    break;
+            if( input<0 ){
+                System.out.println("You did not enter an int>0");
+            }
+            else{
+                //int input=sc.nextInt();
+                int arrayMax=49;
+                int arrayMin=0;
+                while( true ){
                     
+                    if( array2[arrayMax-(arrayMax-arrayMin)/2]==input ){
+                        System.out.println("Your input matched array2 index "+array2[arrayMax-(arrayMax-arrayMin)/2]);
+                        break;
+                    }
+                    else if( array2[arrayMax-((arrayMax-arrayMin)/2)]>input ){
+                        arrayMax=arrayMax-((arrayMax-arrayMin)/2);
+                        //arrayMin=0;
+                    }
+                    else if( array2[arrayMax-((arrayMax-arrayMin)/2)]<input ){
+                        //arrayMax=49;
+                        arrayMin=arrayMax-((arrayMax-arrayMin)/2);
+                    }
+                    if( ( (arrayMax-arrayMin)/2)==0 ){
+                        System.out.println(input+" was not found in the list.");
+                        System.out.println("The number above the input was "+array2[arrayMax]);
+                        System.out.println("The number below the input was "+array2[arrayMin]);
+                        break;
+                        
+                    }
                 }
             }
         }
